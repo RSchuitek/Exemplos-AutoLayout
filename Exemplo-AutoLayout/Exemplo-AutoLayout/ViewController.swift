6a8f159836b9ef40fb8fad6a36e95e4e1e7a5be1
Luiz Rodrigo Schuitek <rodrigoschuitek@gmail.com>
2019-07-18 00:31:35 -0300

    criado exemplo de login e de stack view

---
+++ Exemplo-AutoLayout/Exemplo-AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exemplo-AutoLayout
+//
+//  Created by Luiz Rodrigo Schuitek on 17/07/19.
+//  Copyright © 2019 Luiz Rodrigo Schuitek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
